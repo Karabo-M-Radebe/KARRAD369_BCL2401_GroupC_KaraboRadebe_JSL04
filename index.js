6c55bcf4ae84cff05dc9bd2a264d968be6d8b228
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-03-19 16:51:22 +0200

    second commit

--- index.js
+++ index.js
@@ -41,12 +41,13 @@ console.log(temperatureSuitability);
 // otherwise, print "Better stick to water."
 
 // Define the current hour in 24-hour format
-let currentHour = 22; 
+let currentHour = 22;
 
 // Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
 // Otherwise, print "Better stick to water."
-
+let lateNightSession = currentHour >= 7 && currentHour <=24 ? "Unleash the beast!":"Better stick to water.";
+console.log(lateNightSession);
 
 
 
